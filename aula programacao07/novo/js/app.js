57999b4bb72d36cfdd2204848857b5936cad3e88
WilliamLeite02 <william.a.antunesleite@gmail.com>
2022-05-19 17:24:39 -0300

    materialize

---
+++ aula programacao07/novo/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        fullWildth: true
+        duration: 200
+        
+    });
+  });
