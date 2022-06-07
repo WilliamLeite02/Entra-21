bd1b38bb0dfee057305530bde23954b0aea7d506
williamleite <william.a.antunesleite@gmail.com>
2022-06-07 17:23:27 -0300

    tareffa

---
+++ Professor Max/Bluj/atividades 2/Tarrefa.java
@@ -0,0 +1,34 @@
+//tarefa fazer um calculo nomeFuncionario, salarioBase, inss, valeRefeicao, insalubridade,
+//vendas,salarioLiquido,    salarioLiquido=salarioBase+vendas+insalubridade-inss-valeRefeicao
+//descontos 9% inss  6%valerefeiçao 0,09/0,06/0,22/0,05
+//lucros 22%insalubridade, 5%vendas
+import java.util.Scanner;
+public class Tarrefa{
+    public static void main(String args[]){
+        Scanner ler = new Scanner(System.in);
+        
+        String nomeFuncionario;
+        double salarioBase, inss, valeRefeicao, insalubridade, vendas, salarioLiquido, comissao;
+        
+        System.out.println("nome funcionario:");
+        nomeFuncionario=ler.nextLine();
+        System.out.println("salario base:");
+        salarioBase=ler.nextDouble();
+        System.out.println("total de vendas do funcionário:");
+        vendas=ler.nextDouble();
+        
+        inss=0.09;
+        valeRefeicao=0.06;
+        insalubridade=0.22;
+        comissao=0.05;
+        
+        salarioLiquido= salarioBase+vendas+insalubridade-inss-valeRefeicao;
+        
+        System.out.print("Salario liquido do Funcionario "+nomeFuncionario+" "+salarioLiquido);
+        
+        
+        
+        
+    
+    }
+}
