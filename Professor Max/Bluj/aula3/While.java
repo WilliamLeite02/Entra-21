0a9c2e18b67d353dae42af9591f8926b098f4ba7
William Alexander Antunes Leite <william.leite@acesso.edu>
2022-06-10 17:13:38 -0300

    While e Switch

---
+++ Professor Max/Bluj/aula3/While.java
@@ -0,0 +1,54 @@
+public class While{
+    private int valor;
+    
+    public While(int valor){
+        this.valor = valor;
+    }
+    public void setValor(int valor){
+        this.valor = valor;
+    }
+    public int getValor(){
+        return this.valor;
+    }
+    public int calcularFatorial(){
+        int cont = 1;
+        int fatorial = 1;
+        
+        do{
+            fatorial = fatorial * cont;
+            cont = cont + 1;
+        }while(cont<=this.valor);
+            return fatorial;
+        }
+    public int calcularSomatorio(){
+        int soma = 0;
+        int cont = 1;
+        
+        do{
+            soma = soma +cont;
+            cont ++;
+        }while(cont<=this.valor);
+            return soma;
+        }
+    public int contarMultiplos(){
+        int contMultiplos = 0;
+        int cont =1;
+        
+        do{
+            if(this.valor%cont == 0){
+                contMultiplos++;
+            }
+            cont++;
+        }while(cont<=this.valor);
+        return contMultiplos;
+        }
+    public String verificarSeEPrimo(){
+        int contM = contarMultiplos();
+        
+        if(contM == 2){
+            return this.valor+ " é primo ";
+        }else{
+            return this.valor+" é composto ";
+        }
+    }   
+    }
