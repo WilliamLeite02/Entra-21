d449690faa73b4d82028fe7d95e346dac3ef9a36
William Alexander Antunes Leite <william.leite@acesso.edu>
2022-09-06 17:15:29 -0300

    pagina banco

---
+++ Projeto C/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Banco
+{
+    class BancoX9
+    {
+        public static void Main(string[] args)
+        {
+            double saldo;
+            double saque;
+            double deposito;
+            int numeroConta;
+           
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Bem Vindo Ao Banco X9");
+            Console.WriteLine("");
+            Console.WriteLine("digite o numero da conta: ");
+            numeroConta = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O numero digitado está Correto?");
+
+            string acao = "";
+            acao = Console.ReadLine().ToUpper();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("S = sim");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("N = não");
+            Console.WriteLine("Digite a opção: ");
+
+            while (acao != "S")
+            {
+                if(acao == "N")
+                {
+                    Console.WriteLine("Digite o numero da conta novamente:")
+                    numeroConta= Convert.ToInt32(Console.ReadLine());
+                }
+                else if (acao == "S")
+                {
+                    Console.WriteLine("Digite a Opção desejada:");
+                    Console.WriteLine("== 1 == (Sacar)");
+                    Console.WriteLine("== 2 == (Depositar)");
+                    Console.WriteLine("== 3 == (Saldo)");
+
+                }
+
+            }
+           
+
+
+
+        }
+
+    }
+}
